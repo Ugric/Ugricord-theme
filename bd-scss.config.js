af47222c59d481ddb32950272a7f28cbe89f59a0
William Bell <epicugric@gmail.com>
2023-12-04 18:29:17 +0000

    change to ugricord

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -1,14 +1,15 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
 	meta: {
-		name: 'SoftX',
-		author: 'Gibbu',
+		name: 'Ugricord',
+		author: 'Ugric',
 		version: '1.0.0',
-		description: 'A soft and comfy feel for Discord.',
-		source: 'https://github.com/DiscordStyles/SoftX',
-		invite: 'ZHthyCw'
+		description: 'A soft and comfy feel for Ugricord.',
+		source: 'https://github.com/Ugric/Ugricord-theme',
 	},
-	baseImport: 'https://discordstyles.github.io/SoftX/SoftX.css',
+	dev:"/home/william/.config/Ugricord/Ugricord/themes",
+	dist: 'dist',
+	baseImport: 'https://ugric.github.io/Ugricord-theme/Ugricord.css',
 	addons: [
 		['src/addons/_radialglow.scss', 'dist/RadialGlow.css'],
 		['src/addons/_verticaluserarea.scss', 'dist/VerticalUserArea.css'],
